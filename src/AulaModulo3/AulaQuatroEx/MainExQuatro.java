609188efbcb43cef0650ef50e5594d75ef0a1ffa
Dayvid Dias <dayvid.dias23@gmail.com>
2025-08-06 00:04:23 -0300

    Criação de novos arquivos

--- src/AulaModulo3/AulaQuatroEx/MainExQuatro.java
+++ src/AulaModulo3/AulaQuatroEx/MainExQuatro.java
@@ -1,4 +1,24 @@
 package AulaModulo3.AulaQuatroEx;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class MainExQuatro {
+    public static void main(String[] args) {
+        List<String> listaPolimorfica;
+
+        listaPolimorfica = new ArrayList<>();
+        listaPolimorfica.add("Elemento 1");
+        listaPolimorfica.add("Elemento 2");
+
+        listaPolimorfica = new LinkedList<>();
+        listaPolimorfica.add("Elemento A");
+        listaPolimorfica.add("Elemento B");
+
+        System.out.println("ArrayList: " + listaPolimorfica);
+        System.out.println("LinkedList: " + listaPolimorfica);
+    }
+
+
 }
