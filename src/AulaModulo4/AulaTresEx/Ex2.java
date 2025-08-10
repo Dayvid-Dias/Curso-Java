da3bfd3c5c32cac665f6a957d4e0e7748f007732
Dayvid Dias <dayvid.dias23@gmail.com>
2025-08-10 00:31:23 -0300

    add novos arquivos

--- src/AulaModulo4/AulaTresEx/Ex2.java
+++ src/AulaModulo4/AulaTresEx/Ex2.java
@@ -6,8 +6,14 @@ public class Ex2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite a senha:");
-        String senha = sc.nextLine();
-
+        try {
+            System.out.println("Digite a senha:");
+            String senha = sc.nextLine();
+            if (senha.length() < 8){
+                throw new SenhaInvalidaException("A senha não atende aos requisitos");
+            }
+        } catch (SenhaInvalidaException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ src/AulaModulo4/AulaTresEx/SenhaInvalidaException.java
@@ -0,0 +1,7 @@
+package AulaModulo4.AulaTresEx;
+
+public class SenhaInvalidaException extends RuntimeException {
+    public SenhaInvalidaException(String message) {
+        super(message);
+    }
+}
