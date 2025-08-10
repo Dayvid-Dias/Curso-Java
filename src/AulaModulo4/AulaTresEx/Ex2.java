752b6f2ebc8ed4e0f25fa700aec65cf6e1465783
Dayvid Dias <dayvid.dias23@gmail.com>
2025-08-09 23:52:31 -0300

    add novo arquivo

--- src/AulaModulo4/AulaTresEx/Ex2.java
+++ src/AulaModulo4/AulaTresEx/Ex2.java
@@ -1,7 +1,13 @@
 package AulaModulo4.AulaTresEx;
 
+import java.util.Scanner;
+
 public class Ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite a senha:");
+        String senha = sc.nextLine();
 
-
+    }
 }
