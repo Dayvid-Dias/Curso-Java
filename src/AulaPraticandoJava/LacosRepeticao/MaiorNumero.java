e581ed26d341033cdbbe00fb8eb98b059a325309
Dayvid Dias <dayvid.dias23@gmail.com>
2025-08-20 14:15:55 -0300

    add new files

--- src/AulaPraticandoJava/LacosRepeticao/MaiorNumero.java
+++ src/AulaPraticandoJava/LacosRepeticao/MaiorNumero.java
@@ -1,4 +1,23 @@
 package AulaPraticandoJava.LacosRepeticao;
 
+import java.util.Scanner;
+
 public class MaiorNumero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite os números separados por espaço: ");
+        String[] numerosString = sc.nextLine().split(" ");
+
+        int maiorNumero = Integer.parseInt(numerosString[0]);
+
+        for (int i = 1; i < numerosString.length; i++) {
+            int numero = Integer.parseInt(numerosString[i]);
+            if (numero > maiorNumero) {
+                maiorNumero = numero;
+            }
+        }
+        System.out.println("O maior número é: " + maiorNumero);
+        sc.close();
+    }
 }
